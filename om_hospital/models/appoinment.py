e035983884e01dbef698ea5225c7fd4f6b5f9a7a
birtum <nuevoemail@example.com>
2025-03-10 15:57:40 +0100

    [ADD] buttons for appointment state management, app icon

--- om_hospital/models/appoinment.py
+++ om_hospital/models/appoinment.py
@@ -19,14 +19,45 @@ class HospitalAppointment(models.Model):
     note = fields.Text(
         string="Note",
     )
+    state = fields.Selection(
+        selection=[
+            ("draft", "Draft"),
+            ("confirmed", "Confirmed"),
+            ("ongoing", "Ongoing"),
+            ("done", "Done"),
+            ("canceled", "Canceled"),
+        ],
+        required=True,
+        copy=False,
+        default="draft",
+    )
 
-    
-    # @api.model_create_multi
-    @api.model
+    @api.model_create_multi
+    # @api.model
     def create(self, vals_list):
-        print("odo mates",vals_list)
+        print("odo mates", vals_list)
         for val in vals_list:
-            if not val.get("reference") or val["reference"]=="New":
-               val["reference"]=self.env["ir.sequence"].next_by_code("hospital.appointment")
+            if not val.get("state") or val["state"] == "draft":
+                val["state"] = "confirmed"
+            if not val.get("reference") or val["reference"] == "New":
+                val["reference"] = self.env["ir.sequence"].next_by_code(
+                    "hospital.appointment"
+                )
 
-        return super().create(vals_list)
+        return super(HospitalAppointment, self).create(vals_list)
+
+    def confirm_appointment(self):
+        for object in self:
+            object["state"] = "confirmed"
+
+    def ongoing_appointment(self):
+        for object in self:
+            object["state"] = "ongoing"
+
+    def done_appointment(self):
+        for object in self:
+            object["state"] = "done"
+
+    def cancel_appointment(self):
+        for object in self:
+            object["state"] = "canceled"
